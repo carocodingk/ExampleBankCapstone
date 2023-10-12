2bd42805fee246520e0fbd217963e7ba11bbee66
carocodingk <130165356+carocodingk@users.noreply.github.com>
2023-10-12 02:29:36 -0400

    Added connection to database server

---
+++ dashboard.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+
+from mysql.connector import Error
+from text_variables import welcome
+from secret import db_username, db_password
+
+print(welcome)
+#   Establish connection to local database server
+try:
+    db_connection = mysql.connector.connect(
+        host = 'localhost',
+        database = 'creditcard_capstone',
+        user = db_username,
+        password = db_password
+    )
+    db_cursor = db_connection.cursor()   
+    print('>>>>> Success! Connection to database established')
+except Error as e:
+    print('>>>>> Failed! Unable to establish connection to database server: {}'.format(e))
+finally:
+    if db_connection.is_connected():
+        db_cursor.close()
+        db_connection.close()
+        print('>>>>> Success! Connection to database closed')
+
